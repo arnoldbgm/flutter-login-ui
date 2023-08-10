399a153414abf03fba0793927cfeece4411e0b8d
arnoldbgm <arnold.gallegosm@gmail.com>
2023-08-09 22:12:17 -0500

    Parking v2

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,6 @@ const kColorPrimaryLogin = Color(0xff1E1C2A);
 const kColorSecondaryLogin = Color(0xff00B761);
 const kColorThernaryLogin = Color(0xff25283A);
 
+
+const kColorPrimaryParking = Color(0xff07182B);
+const kColorSecondayParking = Color(0xff0A494F);
--- lib/pages/parking_page.dart
+++ lib/pages/parking_page.dart
@@ -1,30 +1,292 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../constants.dart';
 
 class ParkingPage extends StatelessWidget {
-  const ParkingPage({super.key});
-
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.height);
+    print(MediaQuery.of(context).size.width);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
-              height: 300,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
               width: double.infinity,
-              color: Colors.red,
-              child: SafeArea(
-                child: Column(
-                  children: [
-                    Row(),
-                    Text("Titlte"),
-                    Row(),
+              decoration: BoxDecoration(
+                color: Color(0xff072531),
+                gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                  colors: [
+                    kColorPrimaryParking,
+                    kColorSecondayParking,
                   ],
                 ),
+              ),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Start First Row
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Parkin",
+                          style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "24° C",
+                              style: GoogleFonts.montserrat(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Image.asset(
+                              'assets/images/cloudy.png',
+                              width: 22.0,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    // End First Row
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    //Start Main Text
+                    Container(
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.7,
+                      ),
+                      child: Text(
+                        "Let's find a place for you",
+                        style: GoogleFonts.montserrat(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    // End Main Text
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    // Start Navbar
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Find parking place",
+                              hintStyle: GoogleFonts.montserrat(
+                                  color: Color(0xffC4C8CF),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500),
+                              prefixIcon: Icon(
+                                Icons.search_rounded,
+                                color: Color(0xffC4C8CF),
+                                size: 23.0,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 14.0,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Icon(
+                            Icons.filter_alt_outlined,
+                            color: Colors.white,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xffFDC403),
+                            borderRadius: BorderRadius.circular(10.0),
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomLeft,
+                              end: Alignment.topRight,
+                              colors: [
+                                Color(0xffF8C502),
+                                Color(0xffFCCA04),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 14.0, vertical: 12.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Parking Near You",
+                        style: GoogleFonts.montserrat(
+                          color: kColorPrimaryParking,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "View More",
+                            style: GoogleFonts.montserrat(
+                              color: Color(0xffF8C502),
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_forward_rounded,
+                            color: Color(0xffF8C502),
+                            size: 16.0,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  //Start Card
+                  const SizedBox(height: 22.0,),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    child: Row(
+                      children: [
+                        ItemSliderWidget(),
+                        ItemSliderWidget(),
+                        ItemSliderWidget(),
+                        ItemSliderWidget(),
+                        ItemSliderWidget(),
+
+                      ],
+                    ),
+                  ),
+                  //End Card
+                ],
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ItemSliderWidget extends StatelessWidget {
+  const ItemSliderWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 160.0,
+      margin: const EdgeInsets.only(right: 16.0),
+      child: Column(
+        children: [
+          Container(
+            height: 120.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(14.0),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/maps.png'),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.07),
+                  blurRadius: 10,
+                  offset: const Offset(4, 4),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Blok C Benyamin",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.montserrat(
+                    color: kColorPrimaryParking,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "JL. Kita Berduam 223 22 alksjdhflkjahsdfklajshdflkasjdh",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.montserrat(
+                    color: kColorPrimaryParking.withOpacity(0.55),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 13.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Open ",
+                      style: GoogleFonts.montserrat(
+                        color: Color.fromARGB(205, 40, 156, 63),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "05:00 - 22: 00 asdasd",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.montserrat(
+                          color: kColorPrimaryParking.withOpacity(0.45),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
